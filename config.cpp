38aa0b1b0147a00229992105bdd7802142af996d
Macmachi <38922703+Macmachi@users.noreply.github.com>
2018-05-02 22:04:48 +0200

    First commit

---
+++ config.cpp
@@ -0,0 +1,36 @@
+class CfgPatches {
+    class Missiles {
+        units[] = {
+		"shahab3",
+		"iskander_9K720",
+        }; //
+        weapons[] = {}; 
+        requiredVersion = 1; 
+        requiredAddons[] = {}; // achequer
+    };
+};
+
+class cfgVehicles
+{
+#include"\Ballistic_missiles\missiles.hpp"
+};
+
+class CfgEditorCategories
+{
+	class EdCat_missiles // Category class, you point to it in editorCategory property
+	{
+		displayName = "Ballistic missiles"; // Name visible in the list
+	};
+};
+
+class CfgEditorSubcategories
+{
+	class EdSubcat_missiles_country_iran // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "Iran"; // Name visible in the list
+	};
+	class EdSubcat_missiles_country_russia // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "Russia"; // Name visible in the list
+	};	
+};
---
+++ missiles.hpp
@@ -0,0 +1,115 @@
+class Wreck_base_F;
+
+class shahab3: Wreck_base_F {
+	author = "TILK";
+	mapSize = 7.53;
+	class SimpleObject {
+		eden = 0;
+		animate[] = {};
+		hide[] = {};
+		verticalOffset = 0.829;
+		verticalOffsetWorld = 0;
+		init = "''";
+	};
+	editorPreview = "\Ballistic_missiles\icons\shahab3_icon.paa";
+	//_generalMacro = "Land_Wreck_BMP2_F";
+	scope = 2;
+	scopeCurator = 2;
+	displayName = "Shahab 3";
+	model = "\Ballistic_missiles\objects\shahab3.p3d";
+	icon = "iconObject_1x2";
+    editorCategory = "EdCat_missiles";
+    editorSubcategory = "EdSubcat_missiles_country_iran";
+	destrType = "DestructBuilding";
+	cost = 0;
+	accuracy = 1000;
+	class AttributeValues {
+		objectIsSimple = 0;
+	};
+	
+	// Strategic
+	side = 3;
+	armor = 250;
+	ladders[] = {};
+	// Building
+	nameSound = "obj_building";
+	// Static
+	reversed = 0;
+	hasDriver = 0;
+	audible = 0;
+	simulation = "house";
+	class DestructionEffects {};
+	picture = "pictureStaticObject";
+	weapons[] = {};
+	magazines[] = {};
+	irTarget = 0;
+	type = 1;
+	threat[] = {0, 0, 0};
+	maxSpeed = 0;
+	coefInside = 2;
+	coefInsideHeur = 2;
+	coefSpeedInside = 2;
+	windSockExist = 0;
+	countsForScoreboard = 0;
+	class EventHandlers {
+		class CBA_Extended_EventHandlers {};
+	};
+	
+};
+
+class iskander_9K720: Wreck_base_F {
+	author = "TILK";
+	mapSize = 7.53;
+	class SimpleObject {
+		eden = 0;
+		animate[] = {};
+		hide[] = {};
+		verticalOffset = 0.829;
+		verticalOffsetWorld = 0;
+		init = "''";
+	};
+	editorPreview = "\Ballistic_missiles\icons\shahab3_icon.paa";
+	//_generalMacro = "Land_Wreck_BMP2_F";
+	scope = 2;
+	scopeCurator = 2;
+	displayName = "Iskander 9K720 (SS-26 Stone)";
+	model = "\Ballistic_missiles\objects\iskander_9K720.p3d";
+	icon = "iconObject_1x2";
+    editorCategory = "EdCat_missiles";
+    editorSubcategory = "EdSubcat_missiles_country_russia";
+	destrType = "DestructBuilding";
+	cost = 0;
+	accuracy = 1000;
+	class AttributeValues {
+		objectIsSimple = 0;
+	};
+	
+	// Strategic
+	side = 3;
+	armor = 250;
+	ladders[] = {};
+	// Building
+	nameSound = "obj_building";
+	// Static
+	reversed = 0;
+	hasDriver = 0;
+	audible = 0;
+	simulation = "house";
+	class DestructionEffects {};
+	picture = "pictureStaticObject";
+	weapons[] = {};
+	magazines[] = {};
+	irTarget = 0;
+	type = 1;
+	threat[] = {0, 0, 0};
+	maxSpeed = 0;
+	coefInside = 2;
+	coefInsideHeur = 2;
+	coefSpeedInside = 2;
+	windSockExist = 0;
+	countsForScoreboard = 0;
+	class EventHandlers {
+		class CBA_Extended_EventHandlers {};
+	};
+	
+};
